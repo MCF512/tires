d4a1739464c371adef95ba0246c3365d87ba5896
MCF512 <egdem4enko@yandex.ru>
2021-12-12 14:41:29 +0300

    added cart

--- #src/js/script.js
+++ #src/js/script.js
@@ -7,3 +7,16 @@ headerBtn.addEventListener('click', () => {
   headerDrop.classList.toggle('header__drop--active');
   headerBtn.classList.toggle('header__menu--active')
 });
+
+let cart = {};
+const addBtn = document.querySelectorAll('.card__add');
+const card = document.querySelectorAll('.card');
+
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener('click', (e) => {
+    if (e.target && e.target.className == "card__add") {
+      console.log(i)
+    }
+  });
+}
+
